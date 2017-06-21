f69d703f04045589f789e71561bf4ef441d469bc
Shults-Gavrila <crazy.wdeveloper@yandex.com>
2017-06-21 10:02:09 +1000

    Add Sub News

--- NewParser/Controllers/CnbcController.cs
+++ NewParser/Controllers/CnbcController.cs
@@ -28,6 +28,20 @@ namespace NewParser.Controllers
                 nData.url = img.Attr("data-img-src");
                 newsList.Add(nData);
             }
+            CQ smallArticle = dom.Find("ul.stories_assetlist").Eq(1);
+            for ( int i = 0; i< smallArticle.Children("li").Length; i++)
+            {
+                CQ article = smallArticle.Children("li").Eq(i);
+                CQ data = article.Find("div.headline").Eq(0).Children("a").Eq(0);
+                if (data.Length == 0) continue;
+                newsData nData = new newsData();
+                nData.text = data.Text().ToString();
+                nData.alt_url = data.Attr("href").ToString();
+                nData.url = "";
+                CQ img = article.Find("img").Eq(0);
+                nData.url = img.Attr("data-img-src");
+                newsList.Add(nData);
+            }
             ViewBag.newsList = newsList;
             return View();
         }
@@ -38,6 +52,10 @@ namespace NewParser.Controllers
             ViewBag.title = dom.Find("h1").Eq(0).Text();
 
             string article = dom.Find("#article_body").Eq(0).RenderSelection().ToString();
+            if ( article.Length == 0)
+            {
+                article = dom.Find(".cnbc-body").Eq(0).RenderSelection().ToString();
+            }
             ViewBag.text = article;
             return View();
         }
